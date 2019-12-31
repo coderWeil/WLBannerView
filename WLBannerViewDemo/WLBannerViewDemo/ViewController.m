1fb91f3b8b7472e483887f3a84beede12cc1e3de
liwei <liwei@all-yoga.cn>
2019-12-31 17:23:46 +0800

    将默认的cell和自定义的cell分开展示

--- WLBannerViewDemo/WLBannerViewDemo/ViewController.m
+++ WLBannerViewDemo/WLBannerViewDemo/ViewController.m
@@ -12,28 +12,46 @@
 #import "CustomBannerCell.h"
 
 @interface ViewController ()
-@property (nonatomic, strong) WLBannerView *bannerView;
-@property (nonatomic, strong) NSMutableArray<ImageModel *> *images;
+@property (nonatomic, strong) WLBannerView *bannerView1;
+@property (nonatomic, strong) NSMutableArray<UIImage *> *images1;
+@property (nonatomic, strong) WLBannerView *bannerView2;
+@property (nonatomic, strong) NSMutableArray<ImageModel *> *images2;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.bannerView = [[WLBannerView alloc] initWithCellCls:[CustomBannerCell class]];
-    self.bannerView.bounds = CGRectMake(0, 0, self.view.bounds.size.width, 200);
-    self.bannerView.center = self.view.center;
-    [self.view addSubview:self.bannerView];
-    self.images = @[].mutableCopy;
+    [self _defaultCell];
+    [self _customCell];
+}
+//使用默认的cell展示
+- (void) _defaultCell {
+    self.bannerView1 = [[WLBannerView alloc] init];
+    self.bannerView1.frame = CGRectMake(0, 50, self.view.bounds.size.width, 200);
+    [self.view addSubview:self.bannerView1];
+    self.images1 = @[].mutableCopy;
+    NSArray *names = @[@"01", @"02", @"03"];
+    for (int i = 0; i < names.count; ++i) {
+        [self.images1 addObject:[UIImage imageNamed:names[i]]];
+    }
+    [self.bannerView1 setupData:self.images1];
+    [self.bannerView1 reload];
+}
+//自定义cell展示
+- (void) _customCell {
+    self.bannerView2 = [[WLBannerView alloc] initWithCellCls:[CustomBannerCell class]];
+    self.bannerView2.frame = CGRectMake(0, 300, self.view.bounds.size.width, 200);
+    [self.view addSubview:self.bannerView2];
+    self.images2 = @[].mutableCopy;
     NSArray *names = @[@"01", @"02", @"03"];
     for (int i = 0; i < names.count; ++i) {
         ImageModel *model = [[ImageModel alloc] init];
         model.image = [UIImage imageNamed:names[i]];
-        [self.images addObject:model];
+        [self.images2 addObject:model];
     }
-    [self.bannerView setupData:self.images];
-    [self.bannerView reload];
+    [self.bannerView2 setupData:self.images2];
+    [self.bannerView2 reload];
 }
-
 
 @end
