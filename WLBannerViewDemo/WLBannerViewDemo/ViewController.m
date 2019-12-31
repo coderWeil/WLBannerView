ded2f99a375e22a12faa49a23417fdf452737b48
liwei <liwei@all-yoga.cn>
2019-12-31 17:30:39 +0800

    增加显示网络图片的demo示例

--- WLBannerViewDemo/WLBannerViewDemo/ViewController.m
+++ WLBannerViewDemo/WLBannerViewDemo/ViewController.m
@@ -16,6 +16,8 @@
 @property (nonatomic, strong) NSMutableArray<UIImage *> *images1;
 @property (nonatomic, strong) WLBannerView *bannerView2;
 @property (nonatomic, strong) NSMutableArray<ImageModel *> *images2;
+@property (nonatomic, strong) WLBannerView *bannerView3;
+@property (nonatomic, strong) NSMutableArray<NSString *> *images3;
 @end
 
 @implementation ViewController
@@ -24,6 +26,7 @@
     [super viewDidLoad];
     [self _defaultCell];
     [self _customCell];
+    [self _net];
 }
 //使用默认的cell展示
 - (void) _defaultCell {
@@ -53,5 +56,16 @@
     [self.bannerView2 setupData:self.images2];
     [self.bannerView2 reload];
 }
-
+- (void) _net {
+    self.bannerView3 = [[WLBannerView alloc] init];
+    self.bannerView3.frame = CGRectMake(0, 550, self.view.bounds.size.width, 200);
+    [self.view addSubview:self.bannerView3];
+    self.images3 = @[].mutableCopy;
+    NSArray *urls = @[@"http://image.yoga1314.com/be74dedeca789734632baeeafca85c68.jpeg", @"http://image.yoga1314.com/bb43dd3810760e9d2b3c770d4a1af3fa.jpg", @"http://image.yoga1314.com/77f526b9661b5a33f4bab0993c30e63c.jpg", @"http://image.yoga1314.com/be74dedeca789734632baeeafca85c68.jpeg", @"http://image.yoga1314.com/1f630dd43cfdb63ab47b501c424f699a.jpg", @"http://image.yoga1314.com/77f526b9661b5a33f4bab0993c30e63c.jpg"];
+    for (int i = 0; i < urls.count; ++i) {
+        [self.images3 addObject:urls[i]];
+    }
+    [self.bannerView3 setupData:self.images3];
+    [self.bannerView3 reload];
+}
 @end
